fc5437d185e1c73e03f6f401552d0b97eecb32f1
Choi Hwi bin <chlgnlqls3@nate.com>
2023-12-12 23:11:36 +0900

    feat: 학점계산기 서비스 구현_일급컬렉션,TDD, 객체지향설계

---
+++ 객체지향연습_학점계산기/Grade_Calculator/src/test/java/hwibin/prac/grade_calculator/CourseTest.java
@@ -0,0 +1,17 @@
+package hwibin.prac.grade_calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class CourseTest {
+
+    @DisplayName("과목(코스)를 생성")
+    @Test
+    void createTest(){
+        assertThatCode(() -> new Course("OOP", 3, "A+"))
+                .doesNotThrowAnyException();
+    }
+
+}
